f938c5d48bf5dec9678a2d285b92306ef9f7bf43
megha.mankar <megha.mankar@extentia.com>
2019-11-21 17:03:17 +0530

    Employee class created

---
+++ MegaPower/Contract_MegaPower/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Contract_MegaPower
+{
+    public class Employee
+    {
+        public class StudentData
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string ContactNo { get; set; }
+
+        }
+    }
+}
